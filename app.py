d98a6a298f1f99d9b1483dfa8dd6ea15024e07dc
Griffin Delgado <griffinelijahd@gmail.com>
2019-11-19 15:31:41 -0600

    models and tables are created

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import models
 
 DEBUG = True
 PORT = 8000
@@ -6,14 +7,24 @@ PORT = 8000
 #initialize instance of flask clasas
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+	#connect to DB before every request
+	g.db = models.DATABASE
+	g.db.connect()
+
+@app.after_request
+def after_request():
+	#close connection to DB after every request
+	g.db.close()
+	return response
+
 #default route
 @app.route('/')
 def hello():
 	return 'Hello'
 
 
-
 if __name__ == '__main__':
+	models.initialize()
 	app.run(debug=DEBUG, port=PORT)
-
-
---
+++ models.py
@@ -0,0 +1,40 @@
+import datetime
+from peewee import *
+
+DATABASE = SqliteDatabase('plant_based.sqlite')
+
+class User(Model):
+	username = CharField(unique=True)
+	email = CharField(unique=True)
+	password = CharField()
+
+	class Meta:
+		database = DATABASE
+
+class Comment(Model):
+	user = ForeignKeyField(User, backref='comments')
+	body = CharField()
+	# post = ForeignKeyField(Post, backref='comments')
+
+	class Meta:
+		database = DATABASE
+
+
+class Post(Model):
+	user = ForeignKeyField(User, backref='posts')
+	comment = ForeignKeyField(Comment, backref='posts')
+	title = CharField()
+	description = CharField()
+	image = CharField()
+	created = DateTimeField(default=datetime.datetime.now)
+
+	class Meta:
+		database = DATABASE
+
+
+def initialize():
+	DATABASE.connect()
+	DATABASE.create_tables([User, Comment, Post], safe=True)
+	print('TABLES CREATED')
+	DATABASE.close()
+
