93c075cec708d2a6652be222e1a83a329a78d233
Griffin Delgado <griffinelijahd@gmail.com>
2019-11-19 18:41:10 -0600

    user can login with proper authentication

--- resources/users.py
+++ resources/users.py
@@ -33,3 +33,42 @@ def register():
 		user_dict = model_to_dict(user)
 		del user_dict['password']
 		return jsonify(data=user_dict, status={'code': 201, 'message': 'Succesfully registered user {}'.format(user_dict['email'])}),  201
+
+#route to let users login
+@users.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	#first look up user by their email
+	try:#if match is found turn to dict and proceed
+		user = models.User.get(models.User.email == payload['email'])
+		user_dict = model_to_dict(user)
+		#check if pw hash matches pw in db
+		if(check_password_hash(user_dict['password'], payload['password'])):
+			#if the pw matches the user in the db log them in
+			login_user(user)
+			#remove users password before returning the object
+			del user_dict['password']
+			return jsonify(data=user_dict, status={'code': 200, 'message': 'Succesfully logged in {}'.format(user_dict['email'])}), 200
+		else: #if pw dose match user's pw in the db return error
+			return jsonify(data={}, status={'code': 401, 'message': 'Username or Password is incorrect'}), 401
+
+	except models.DoesNotExist:
+		return jsonify(data={}, status={'code': 401, 'message': 'Username or Password is incorrect'}), 401
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
